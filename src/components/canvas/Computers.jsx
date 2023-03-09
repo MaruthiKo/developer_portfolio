d323b4e8a4c1f009ccce439f7c0c01c7eff777aa
MaruthiKo <marurohi@gmail.com>
2023-03-10 03:55:51 +0530

    hero section text completed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControl, Preload, useGLTF } from '@react-three/drei';
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15}
+      groundColor="black" />
+    </mesh>
   )
 }
 
